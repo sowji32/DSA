041c2385fd0afea95ee2f6ec4c488d9e0164d400
sowji32 <koppulalakshmisowjanya@gmail.com>
2025-10-28 07:20:55 +0530

    Implement Stack class with basic operations

---
+++ DSA/stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+        return "Stack is empty"
+
+    def peek(self):
+        if not self.is_empty():
+            return self.stack[-1]
+        return "Stack is empty"
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+s = Stack()
+s.push(10)
+s.push(20)
+print(s.pop())   # 20
+print(s.peek())  # 10
